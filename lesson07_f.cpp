44603090e40d593fbfc7e5792a9fe5bec0eed9b8
fujiwara <fujiwara-at@njk.co.jp>
2017-09-06 09:28:32 +0900

    lesson07_f 新規作成

---
+++ lesson07_f.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<pthread.h>
+
+using namespace std;
+
+void *PrintHelloWorld(void* arg)
+{
+	cout << "HelloWorld!" << endl;
+}
+
+int main(void)
+{
+	pthread_t thread;
+	pthread_create(&thread, NULL, PrintHelloWorld,(void *)NULL);
+	pthread_join(thread, NULL);
+	return 0;
+}
+
